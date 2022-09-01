a4a7ff26d7ce54c7d5c44ed871fe9dfc5494d8c7
Kofi Poku <kpoku@andrew.cmu.edu>
2022-09-01 04:29:44 -0400

    Initial commit/Gotta get a move on

---
+++ pages/index.tsx
@@ -0,0 +1,131 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import formatDate from 'pliny/utils/formatDate'
+import { sortedBlogPost, allCoreContent } from 'pliny/utils/contentlayer'
+import { InferGetStaticPropsType } from 'next'
+import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { allBlogs } from 'contentlayer/generated'
+import type { Blog } from 'contentlayer/generated'
+
+import AdinkraLogo from '@/components/AdinkraLogo'
+import { useTheme } from 'next-themes'
+  
+
+const MAX_DISPLAY = 5
+
+export const getStaticProps = async () => {
+  const sortedPosts = sortedBlogPost(allBlogs) as Blog[]
+  const posts = allCoreContent(sortedPosts)
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
+  
+  const { theme, setTheme, resolvedTheme } = useTheme(); // Not sure if this is the best way to go about it...
+  
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      
+      <div>
+
+        <div className="flex flex-col items-center my-6 xl:flex-row gap-x-12 xl:mb-12">
+          <div className="pt-6">
+            <h1 className="pb-6 font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+                style={{fontSize: '2.8rem'}}
+            >
+              Kofi's Blog
+            </h1>
+            <h2 className="text-lg prose text-gray-600 dark:text-gray-400">
+              [Something else]
+            To that end, I plan to use this mostly for academic and technical writing. <i>But</i> I can't promise there won't be an attempt at a philosophical post every now and then.
+            Hopefully others find some use in my writing as well.
+            </h2>
+          </div>
+          <div className="flex items-center justify-center mx-2 my-12 w-96">
+            <AdinkraLogo mode={ (theme === 'light' || resolvedTheme === 'light') ? "lightMode" : "darkMode"} />
+          </div>
+        </div>
+
+      </div>
+      
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <h2 className="pb-6 font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl"
+            style={{fontSize: '2.5rem'}}
+        >
+            Latest
+        </h2>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((post) => {
+            const { slug, date, title, summary, tags } = post
+            return (
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider && (
+        <div className="flex items-center justify-center pt-4">
+          <NewsletterForm />
+        </div>
+      )}
+    </>
+  )
+}
