9116f6fc905ee6066fdca58829b77b71abf36719
Kofi Poku <kpoku@andrew.cmu.edu>
2022-09-03 18:16:58 -0400

    Rollback blog to previous installation version (through degit, NO pliny...)

---
+++ README.md
@@ -0,0 +1,1 @@
+Adapted from https://github.com/timlrx/tailwind-nextjs-starter-blog
---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  // { href: '/projects, title: 'Projects' },
+  { href: 'https://www.kofipoku.com/portfolio', title: 'Projects' },
+  { href: 'https://www.kofipoku.com/', title: 'Home' },
+]
+
+export default headerNavLinks
